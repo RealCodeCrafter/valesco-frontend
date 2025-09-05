170fe1af86e4fb5d5ca9c24c197f94d203e7cb89
Vali <valireyimbergenov79@gmail.com>
2025-09-05 14:56:50 +0500

    main.ts update

--- src/context/api/index.js
+++ src/context/api/index.js
@@ -4,7 +4,7 @@ import { logout } from "../slices/authSlice";
 const baseQuery = async(args, api, extraOptions) => {
     const { dispatch } = api;
     const rawBaseQuery = fetchBaseQuery({
-        baseUrl: "https://www.valescooil.com",
+        baseUrl: "http://localhost:8000",
         prepareHeaders: (headers) => {
             const token = localStorage.getItem("x-auth-token");
             if (token) {
