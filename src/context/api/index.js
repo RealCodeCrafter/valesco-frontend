26bef8c99c5ddcf6fff8a7c6e9635905208a5792
Vali <valireyimbergenov79@gmail.com>
2025-09-05 21:22:59 +0500

    main.ts update

--- src/context/api/index.js
+++ src/context/api/index.js
@@ -4,7 +4,7 @@ import { logout } from "../slices/authSlice";
 const baseQuery = async(args, api, extraOptions) => {
     const { dispatch } = api;
     const rawBaseQuery = fetchBaseQuery({
-        baseUrl: "http://localhost:10001",
+        baseUrl: "http://localhost:10000",
         prepareHeaders: (headers) => {
             const token = localStorage.getItem("x-auth-token");
             if (token) {
